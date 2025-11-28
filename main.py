59a9b706643bb8fbe5596af8b2caa6d00810dea4
Haraprasanna Misra <haraprasannamisra@gmail.com>
2025-11-29 01:15:55 +0530

    Add main.py

--- main.py
+++ main.py
@@ -1,72 +1,64 @@
-from urllib.parse import urlparse, urlunparse, urlencode, quote, unquote, parse_qs
+import streamlit as st
+from urlHandler import urlHandler
 
-class urlHandler():
-    def __init__(self, url: str):
-        self.url = url
-        self.parsed_url = urlparse(url)
-        self.placeHolderUrl = None
+def createBasicPageLayout(title: str) -> tuple:
+    """
+    Defines the basic layout of the streamlit application
     
-    @property
-    def fields(self) -> list | None:
-        """
-        If their are fields available in the url then return them otherwise returns None
+    :param title: Defines the titke of the streamlit application
+    :type title: str
+    :return: A tuple containing the URL entered by the user, the sections (info and placeHolder) and a flag for showing or hiding the filled URL
+    :rtype: tuple
+    """
+
+    st.title(title)
+    url = st.text_input("Enter the url", key="urlInput")
+    infoSec, placeHolderSec = st.columns(2)
+    showFilledURL = st.checkbox("Show filled URL")
+    return url, infoSec, placeHolderSec, showFilledURL
+
+def fillInfoSection(urlHandlerObject: urlHandler):
+    """
+    Deals with filling the info section
+    
+    :param urlHandlerObject: The object whose information will be displayed
+    :type urlHandlerObject: urlHandler
+    """
+
+    st.header("URL details")
+
+    with st.expander("Object"):
+        st.write(urlHandlerObject.json)
         
-        :param self: The object which is calling the function
-        :return: A list of fields if present in the query paramiters of the URL otherwise returns None
-        :rtype: list | None
-        
-        """
-        query = self.parsed_url.query
-        if query:
-            keyValuePairs = parse_qs(query)
-            return list(keyValuePairs.keys())
-        return None
+    with st.expander("Parts"):
+        st.write(urlHandlerObject.parts)
+
+def fillPlaceHolders(fields: list | None, urlHandlerObject: urlHandler):
+    """
+    Defines the placeholder form
     
-    @property
-    def parts(self) -> dict:
-        """
-        Returns all the parts of the given URL as a dictonary. Return None for those parts which are not available
-        
-        :param self: The object which is calling the function
-        :return: A python dictionary containing dufferent parts of the URL such as Scheme, Network location, Path, Query, Fragment
-        :rtype: dict
-        """
-        return {
-            "Scheme" : self.parsed_url.scheme,
-            "Network location" : self.parsed_url.netloc,
-            "Path" : self.parsed_url.path,
-            "Query" : self.parsed_url.query,
-            "Fragment" : self.parsed_url.fragment
-        }
-    
-    def definePlaceHolders(self, placeHolderNames: list) -> str :
-        Scheme, netloc, Path, _, Fragment = self.parts.values()
-        fields = self.fields
-        queryParams = [fields[i] + "={" + placeHolderNames[i] + "}" for i in range(len(fields))]
-        queryParamsstr = "&".join(queryParams)
-        components = (Scheme, netloc, Path, '', queryParamsstr, Fragment)
-        placeHolderUrl = urlunparse(components)
-        self.placeHolderUrl = placeHolderUrl
-        return placeHolderUrl
-    
-    def __str__(self):
-        return f"""
-        URL : {self.url}
-        Fields : {", ".join(self.fields) or None}
-        Parts :
-            Scheme: {self.parsed_url.scheme}
-            Netloc: {self.parsed_url.netloc}
-            Path: {self.parsed_url.path or None}
-            Query: {self.parsed_url.query or None}
-            Fragment: {self.parsed_url.fragment or None}
-        URL with placeholders : {self.placeHolderUrl}
-""" # Display None if a part in the URL is not preset
+    :param fields: The fields in the query parameters
+    :type fields: list | None
+    :param urlHandlerObject: The object containing the definePlaceHolders method
+    :type urlHandlerObject: urlHandler
+    """
+
+    st.header("Placeholder names")
+    if isinstance(fields, list):
+        placeHolderNames = [st.text_input(f"Placrholder name for '{field}' field", key=f"placeholder_{key}") for key, field in enumerate(fields, start=1)]
+        urlHandlerObject.definePlaceHolders(placeHolderNames=placeHolderNames)
+    else:
+        st.write("Their are no Query paramitters in the given URL")
 
 if __name__ == "__main__":
-    # url = "https://www.example.com/path/to/resource?name=John%20Doe&age=30#section1"
-    url = "https://www.google.com/search?q={Query}"
-    URL = urlHandler(url)
-    URL.definePlaceHolders(["Query","Page"])
-    print(URL)
-    # print(URL.fields)
-    # print(URL.parts)
+    url, infoSec, placeHolderSec, showFilledURL = createBasicPageLayout(title="URL filler")
+    urlHandlerObj = urlHandler(url)
+    if url:
+        with infoSec:
+            fillInfoSection(urlHandlerObject=urlHandlerObj)
+        
+        with placeHolderSec:
+            fillPlaceHolders(fields=urlHandlerObj.fields, urlHandlerObject=urlHandlerObj)
+    
+        if showFilledURL:
+            st.text(urlHandlerObj.placeHolderUrl)
